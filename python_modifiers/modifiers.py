7b89975859c7212b342eda2a84795fead556725e
TechAngle <64697531+TechAngle@users.noreply.github.com>
2024-10-12 16:51:01 +0300

    Fixed logic

--- python_modifiers/modifiers.py
+++ python_modifiers/modifiers.py
@@ -1,14 +1,33 @@
 import inspect
 from functools import wraps
-from .exceptions.exceptions import PrivateFunctionException, ProtectedFunctionException
+from .exceptions.exceptions import ModifierOutsideOfClassException, PrivateFunctionException, ProtectedFunctionException
 from ._messages import LEVEL_EXCEPTION
 
 def public(func):
     """ Decorator for public function """
     return func
 
+def __has_self_param(func):
+    """
+    Checks for 'self' param in function
+
+    Args:
+        func (function): function
+
+    Raises:
+        ModifierOutsideOfClassException: if `self` not found
+    """
+    # Checking for self param
+    sigs = inspect.signature(func)
+    isClassFunc = 'self' in sigs.parameters  # Update this line
+    
+    if not isClassFunc:
+        raise ModifierOutsideOfClassException("Modifiers can only be used inside a class!")
+
 def private(func):
     """ Decorator for private function """
+    __has_self_param(func)
+    
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         caller_frame = inspect.stack()[1]
@@ -23,6 +42,8 @@ def private(func):
 
 def protected(func):
     """ Decorator for protected function """
+    __has_self_param()
+    
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         caller_frame = inspect.stack()[1]

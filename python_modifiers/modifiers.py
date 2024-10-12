95d1c38b96aef10ba3ae898288723d4a32d70e2d
TechAngle <64697531+TechAngle@users.noreply.github.com>
2024-10-12 16:24:10 +0300

    Rewrited modifiers logic

--- python_modifiers/modifiers.py
+++ python_modifiers/modifiers.py
@@ -1,40 +1,37 @@
-""" This is the main project file which contains private, protected, and public modifiers """
 import inspect
 from functools import wraps
 from .exceptions.exceptions import PrivateFunctionException, ProtectedFunctionException
 from ._messages import LEVEL_EXCEPTION
 
 def public(func):
-    """ This modifier makes function available for everyone """
+    """ Decorator for public function """
     return func
 
 def private(func):
-    """ Makes function private, callable only within the class """
+    """ Decorator for private function """
     @wraps(func)
-    def private_function_wrapper(self, *args, **kwargs):
-        current_file = inspect.getfile(func)
+    def wrapper(self, *args, **kwargs):
         caller_frame = inspect.stack()[1]
-        caller_file = caller_frame.filename
         caller_self = caller_frame.frame.f_locals.get('self', None)
 
-        # Check if the caller is from the same class
-        if current_file != caller_file or (caller_self and not isinstance(caller_self, self.__class__)):
+        if caller_self is None or not isinstance(caller_self, self.__class__):
             raise PrivateFunctionException(LEVEL_EXCEPTION)
         
         return func(self, *args, **kwargs)
-    return private_function_wrapper
+
+    return wrapper
 
 def protected(func):
-    """ Makes function protected, callable within the class or subclasses """
+    """ Decorator for protected function """
     @wraps(func)
-    def protected_function_wrapper(self, *args, **kwargs):
-        current_frame = inspect.currentframe()
-        caller_frame = inspect.getouterframes(current_frame, 2)[1].frame
-        caller_self = caller_frame.f_locals.get('self', None)
+    def wrapper(self, *args, **kwargs):
+        caller_frame = inspect.stack()[1]
+        caller_self = caller_frame.frame.f_locals.get('self', None)
 
-        # Check if the caller is from the same class or a subclass
-        if caller_self is None or (caller_self.__class__ != self.__class__ and not isinstance(caller_self, type(self))):
+        if caller_self is None or not isinstance(caller_self, (self.__class__, *self.__class__.__subclasses__)):
             raise ProtectedFunctionException(LEVEL_EXCEPTION)
 
         return func(self, *args, **kwargs)
-    return protected_function_wrapper
+
+    return wrapper
+
